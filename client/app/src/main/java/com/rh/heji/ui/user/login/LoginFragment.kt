2ab9f451169a71c864e0c9ff58851b4d768f86ec
ruanhao <52927295@qq.com>
2021-01-30 12:04:37 +0800

    user register/login

--- client/app/src/main/java/com/rh/heji/ui/user/login/LoginFragment.kt
+++ client/app/src/main/java/com/rh/heji/ui/user/login/LoginFragment.kt
@@ -1,7 +1,8 @@
 package com.rh.heji.ui.user.login
 
-import android.content.Context
 import android.view.View
+import androidx.activity.OnBackPressedCallback
+import androidx.drawerlayout.widget.DrawerLayout
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.Navigation
@@ -9,29 +10,37 @@ import com.blankj.utilcode.util.LogUtils
 import com.rh.heji.R
 import com.rh.heji.databinding.LoginFragmentBinding
 import com.rh.heji.ui.base.BaseFragment
-import kotlinx.android.synthetic.main.app_bar_main.*
 
 class LoginFragment : BaseFragment() {
 
-    private lateinit var viewModel: LoginViewModel
     lateinit var binding: LoginFragmentBinding;
+    private val viewModel by lazy { ViewModelProvider(this).get(LoginViewModel::class.java) }
 
-    override fun onAttach(context: Context) {
-        super.onAttach(context)
-        viewModel = ViewModelProvider(this).get(LoginViewModel::class.java)
+    override fun layoutId(): Int {
+        return R.layout.login_fragment
     }
 
-    override fun onStart() {
-        super.onStart()
-        mainActivity.toolbar.visibility =View.GONE
+    override fun setUpViews() {
+        super.setUpViews()
+        mainActivity.drawer.setDrawerLockMode(DrawerLayout.LOCK_MODE_LOCKED_CLOSED)
+        mainActivity.fab.visibility = View.GONE
+        mainActivity.toolbar.visibility = View.GONE
+        //拦截回退直接退出  object : Class 内部类
+        mainActivity.onBackPressedDispatcher.addCallback(this, object : OnBackPressedCallback(true) {
+            override fun handleOnBackPressed() {
+                mainActivity.finish()
+            }
+        })
     }
 
-    override fun onDestroyView() {
-        mainActivity.toolbar.visibility =View.VISIBLE
-        super.onDestroyView()
+    override fun restoreVies() {
+        super.restoreVies()
+        mainActivity.toolbar.visibility = View.VISIBLE
+        mainActivity.fab.visibility = View.VISIBLE
+        mainActivity.drawer.setDrawerLockMode(DrawerLayout.LOCK_MODE_UNDEFINED)
     }
+
     override fun initView(view: View?) {
-
         view?.let { v ->
             binding = LoginFragmentBinding.bind(v);
             binding.tvRegister.setOnClickListener {
@@ -49,11 +58,6 @@ class LoginFragment : BaseFragment() {
                         })
             }
         }
-
-    }
-
-    override fun layoutId(): Int {
-        return R.layout.login_fragment
     }
 
 }

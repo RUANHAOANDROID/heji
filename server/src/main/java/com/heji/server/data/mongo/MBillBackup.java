17724fc0c23495ce235c9a0c269e894a1e7c85c2
ruanhao <52927295@qq.com>
2021-02-01 16:47:03 +0800

    删除 mysql 相关类,使用Mongodb

---
+++ server/src/main/java/com/heji/server/data/mongo/MBillBackup.java
@@ -0,0 +1,66 @@
+package com.heji.server.data.mongo;
+
+import com.heji.server.module.BillModule;
+import lombok.Data;
+import lombok.ToString;
+import lombok.experimental.Accessors;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.mongodb.core.mapping.Field;
+
+import static com.heji.server.data.mongo.MBill.COLLECTION_NAME;
+
+//@Transient 普通字段注解
+@Data//get set
+@ToString//tostring
+@Accessors(chain = true)//能让我们方便使用链式方法创建实体对象。
+@Document(COLLECTION_NAME)
+public class MBillBackup {
+    public static final String COLLECTION_NAME = "bill";
+    @Id
+    private String _id;
+    //账本ID
+    private String bookId;
+
+    private Double money;
+
+    private String category;
+
+    private Integer type;
+
+    private String dealer;
+
+    private String createUser;
+
+    private String remark;
+
+    private String[] images;
+
+    private long time;//选择的时间
+
+    @CreatedDate
+    @Field(name = "create_time")
+    private long createTime;
+
+    @Field(name = "update_time")
+    private long updateTime;
+
+    public MBillBackup() {
+
+    }
+
+    public MBillBackup(BillModule billModule) {
+        _id = billModule.get_id();
+        bookId = billModule.getBookId();
+        money = billModule.getMoney();
+        category = billModule.getCategory();
+        type = billModule.getType();
+        dealer = billModule.getDealer();
+        remark = billModule.getRemark();
+        time = billModule.getTime();
+        createTime = billModule.getCreateTime();
+        updateTime = billModule.getUpdateTime();
+        createUser = billModule.getCreateUser();
+    }
+}
--- server/src/main/java/com/heji/server/data/mysql/repository/UserDao.java
+++
@@ -1,8 +0,0 @@
-package com.heji.server.data.mysql.repository;
-
-import com.heji.server.data.mysql.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserDao extends JpaRepository<User, Integer> {
-
-}

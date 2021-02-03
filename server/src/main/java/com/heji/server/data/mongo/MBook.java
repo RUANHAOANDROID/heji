cccd0df6994afa255f23d2581146b503c017db4c
ruanhao <52927295@qq.com>
2021-02-03 18:48:43 +0800

    账单删除接口

--- server/src/main/java/com/heji/server/data/mongo/MBook.java
+++ server/src/main/java/com/heji/server/data/mongo/MBook.java
@@ -3,11 +3,9 @@ package com.heji.server.data.mongo;
 import lombok.Data;
 import lombok.ToString;
 import lombok.experimental.Accessors;
-import org.bson.types.ObjectId;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 import org.springframework.data.mongodb.core.mapping.Field;
-import org.springframework.data.mongodb.core.mapping.MongoId;
 
 import static com.heji.server.data.mongo.MBook.COLLATION_NAME;
 

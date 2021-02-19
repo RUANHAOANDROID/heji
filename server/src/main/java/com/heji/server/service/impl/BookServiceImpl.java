eb4311448b81b317de518c38ed6546e931e3b8a0
ruanhao <52927295@qq.com>
2021-02-19 10:59:37 +0800

    新增账本,修改账本ID

--- server/src/main/java/com/heji/server/service/impl/BookServiceImpl.java
+++ server/src/main/java/com/heji/server/service/impl/BookServiceImpl.java
@@ -1,12 +1,17 @@
 package com.heji.server.service.impl;
 
+import com.heji.server.data.mongo.BaseMongoTemplate;
 import com.heji.server.data.mongo.MBook;
 import com.heji.server.data.mongo.repository.MBookRepository;
 import com.heji.server.service.BookService;
+import org.bson.Document;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.index.CompoundIndexDefinition;
+import org.springframework.data.mongodb.core.index.IndexOperations;
 import org.springframework.stereotype.Service;
 
 @Service("BookService")
-public class BookServiceImpl implements BookService {
+public class BookServiceImpl extends BaseMongoTemplate implements BookService {
     final MBookRepository mBookRepository;
 
     public BookServiceImpl(MBookRepository mBookRepository) {
@@ -31,5 +36,15 @@ public class BookServiceImpl implements BookService {
     @Override
     public MBook updateBook(MBook book) {
         return mBookRepository.save(book);
+    }
+
+    @Override
+    protected void init() {
+        MongoTemplate mongoTemplate = getMongoTemplate();
+        if (!mongoTemplate.collectionExists(MBook.COLLATION_NAME)) {
+            mongoTemplate.createCollection(MBook.COLLATION_NAME);
+            IndexOperations indexOpe = mongoTemplate.indexOps(MBook.COLLATION_NAME);
+            indexOpe.ensureIndex(new CompoundIndexDefinition(new Document("_id", "hashed")));
+        }
     }
 }

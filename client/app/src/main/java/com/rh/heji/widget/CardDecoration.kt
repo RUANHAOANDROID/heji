64effa0a6fce7f48d411f572a6cdd21faf64b18c
RH <52927295@qq.com>
2021-04-16 18:45:05 +0800

    每次度设置BG避免Item视图重用导致的圆角.预添加没有更多视图

--- client/app/src/main/java/com/rh/heji/widget/CardDecoration.kt
+++ client/app/src/main/java/com/rh/heji/widget/CardDecoration.kt
@@ -2,12 +2,15 @@ package com.rh.heji.widget
 
 import android.content.res.Resources
 import android.graphics.Canvas
+import android.graphics.Color
 import android.graphics.Rect
+import android.graphics.drawable.ColorDrawable
 import android.util.TypedValue
 import android.view.View
 import androidx.core.content.ContextCompat
 import androidx.recyclerview.widget.RecyclerView
 import androidx.recyclerview.widget.RecyclerView.ItemDecoration
+import com.chad.library.adapter.base.BaseNodeAdapter
 import com.lxj.xpopup.util.XPopupUtils
 import com.rh.heji.R
 import com.rh.heji.ui.bill.adapter.NodeBillsAdapter
@@ -16,18 +19,33 @@ import com.rh.heji.ui.bill.adapter.TYPE_TITLE
 class CardDecoration(val padding: Int = 8) : ItemDecoration() {
     private fun drawCardBackground(c: Canvas, parent: RecyclerView) {
         if (adapterEmpty(parent)) return
+        val adapter = parent.adapter as BaseNodeAdapter
         val childCount = parent.childCount
         for (i in 0 until childCount) {
             val child = parent.getChildAt(i) // Item View
             val params = child.layoutParams as RecyclerView.LayoutParams
             val position = params.viewAdapterPosition
-            if (parent.adapter!!.getItemViewType(position) == TYPE_TITLE) {
-                child.background = XPopupUtils.createDrawable(ContextCompat.getColor(parent.context, R.color._xpopup_light_color), 15f, 15f, 0f, 0f)
-            } else if (isLastInItemList(parent, position) || isLastItem(parent, position)) {
+            //val currentItem = adapter.getItem(position)//当前的
+
+            if (isLast(parent.adapter, position)) {//最后一个
                 child.background = XPopupUtils.createDrawable(ContextCompat.getColor(parent.context, R.color._xpopup_light_color), 0f, 0f, 15f, 15f)
+                return //可能会越界最后一个绘制完不再搜选下一个
+            }
+            //val nextItem = adapter.getItem(position + 1)//可能会越界
+            when {
+                adapter.getItemViewType(position) == TYPE_TITLE -> {
+                    child.background = XPopupUtils.createDrawable(ContextCompat.getColor(parent.context, R.color._xpopup_light_color), 15f, 15f, 0f, 0f)
+                }
+                isItemLast(parent, position) -> {
+                    child.background = XPopupUtils.createDrawable(ContextCompat.getColor(parent.context, R.color._xpopup_light_color), 0f, 0f, 15f, 15f)
+                }
+                else -> {
+                    child.background =ColorDrawable(Color.WHITE)//每次度设置BG避免Item视图重用导致的圆角
+                }
             }
         }
     }
+
 
     private fun adapterEmpty(parent: RecyclerView): Boolean {
         val adapter: NodeBillsAdapter = parent.adapter as NodeBillsAdapter
@@ -51,7 +69,7 @@ class CardDecoration(val padding: Int = 8) : ItemDecoration() {
             // header
             outRect[0, (padding / 2), 0] = 0
         } else {
-            if (isLastInItemList(parent, position) || isLastItem(parent, position)) {
+            if (isItemLast(parent, position) || isLast(parent.adapter, position)) {
                 // last item before next header
                 outRect[0, 0, 0] = (padding / 2)
             }
@@ -59,18 +77,17 @@ class CardDecoration(val padding: Int = 8) : ItemDecoration() {
         //        outRect.inset((int) size16dp, 0);
         outRect.left = padding
         outRect.right = padding
-        if (isLastItem(parent, position)) { //最后一个
+        if (isLast(parent.adapter, position)) { //最后一个
             outRect.bottom = padding / 2
         }
     }
 
-    private fun isLastItem(parent: RecyclerView, position: Int): Boolean {
-        return position == parent.adapter!!.itemCount - 1
-    }
 
-    private fun isLastInItemList(parent: RecyclerView, position: Int): Boolean {
+    private fun isItemLast(parent: RecyclerView, position: Int): Boolean {
         return parent.adapter!!.getItemViewType(position + 1) == TYPE_TITLE
     }
+    private fun isLast(adapter: RecyclerView.Adapter<RecyclerView.ViewHolder>?, position: Int) =
+            (position == adapter!!.itemCount - 1)
 
     private fun getPadding(resources: Resources): Int {
         val size16dp = padding * 1f

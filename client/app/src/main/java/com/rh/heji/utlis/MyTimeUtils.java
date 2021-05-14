73eabbb2b68154c4c6ae6986cb1c5d1c49555505
RH <52927295@qq.com>
2021-05-14 16:44:51 +0800

    替换为真实数据

--- client/app/src/main/java/com/rh/heji/utlis/MyTimeUtils.java
+++ client/app/src/main/java/com/rh/heji/utlis/MyTimeUtils.java
@@ -67,4 +67,16 @@ public class MyTimeUtils {
         String strLastDay = sdf.format(calendar.getTime());
         return strLastDay;
     }
+
+    public static int getMonthLastDay(int year, int month)
+    {
+        Calendar a = Calendar.getInstance();
+        a.set(Calendar.YEAR, year);
+        a.set(Calendar.MONTH, month - 1);
+        a.set(Calendar.DATE, 1);//把日期设置为当月第一天
+        a.roll(Calendar.DATE, -1);//日期回滚一天，也就是最后一天
+        int maxDate = a.get(Calendar.DATE);
+        return maxDate;
+    }
+
 }

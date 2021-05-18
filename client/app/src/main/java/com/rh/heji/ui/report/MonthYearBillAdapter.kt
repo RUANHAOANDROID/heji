9fc1690c838ef98ab33a3e5bc51a52cb15957736
RH <52927295@qq.com>
2021-05-18 19:33:19 +0800

    完善统计列表

--- client/app/src/main/java/com/rh/heji/ui/report/MonthYearBillAdapter.kt
+++ client/app/src/main/java/com/rh/heji/ui/report/MonthYearBillAdapter.kt
@@ -3,26 +3,23 @@ package com.rh.heji.ui.report
 import com.chad.library.adapter.base.BaseQuickAdapter
 import com.chad.library.adapter.base.viewholder.BaseViewHolder
 import com.rh.heji.R
-import com.rh.heji.data.db.Bill
+import com.rh.heji.data.db.query.IncomeTimeSurplus
 import com.rh.heji.databinding.ItemMonthYearBillBinding
+import java.math.BigDecimal
 
 /**
  *Date: 2021/5/18
  *Author: 锅得铁
  *#
  */
-class MonthYearBillAdapter(data: MutableList<Bill>?) :
-    BaseQuickAdapter<Bill, BaseViewHolder>(layoutResId = R.layout.item_month_year_bill, data) {
+class MonthYearBillAdapter(data: MutableList<IncomeTimeSurplus>?) :
+    BaseQuickAdapter<IncomeTimeSurplus, BaseViewHolder>(layoutResId = R.layout.item_month_year_bill, data) {
     lateinit var itemBinding: ItemMonthYearBillBinding
-    override fun convert(holder: BaseViewHolder, item: Bill) {
+    override fun convert(holder: BaseViewHolder, item: IncomeTimeSurplus) {
         itemBinding = ItemMonthYearBillBinding.bind(holder.itemView)
-        itemBinding
+        itemBinding.tvDate.text =item.time
+        itemBinding.tvIncome.text =if (!item.income.toString().contains(".")) "${item.income}.00" else item.income.toString()
+        itemBinding.tvExpenditure.text =if (!item.expenditure.toString().contains(".")) "${item.expenditure}.00" else item.expenditure.toString()
+        itemBinding.tvSurplus.text =if (!item.surplus.toString().contains(".")) "${item.surplus}.00" else item.surplus.toString()
     }
 }
-
-data class MonthYearBill(
-    var date: String,
-    var income: String,
-    var exception: String,
-    var surplus: String
-)

1b14eea9c88ec2c40738fed74be5977ba5ee62f5
RH <52927295@qq.com>
2021-05-19 18:12:10 +0800

    优化统计图表

--- client/app/src/main/java/com/rh/heji/ui/report/CategoryTotalAdapter.kt
+++ client/app/src/main/java/com/rh/heji/ui/report/CategoryTotalAdapter.kt
@@ -1,15 +1,12 @@
 package com.rh.heji.ui.report
 
-import android.R.color
-import android.graphics.drawable.ClipDrawable
-import android.graphics.drawable.ColorDrawable
-import android.view.Gravity
 import com.chad.library.adapter.base.BaseQuickAdapter
 import com.chad.library.adapter.base.viewholder.BaseViewHolder
 import com.github.mikephil.charting.data.PieEntry
 import com.github.mikephil.charting.formatter.PercentFormatter
 import com.rh.heji.R
 import com.rh.heji.databinding.ItemCategoryTotalBinding
+import com.rh.heji.utlis.ColorUtils
 import com.rh.heji.utlis.textdraw.ColorGenerator
 import com.rh.heji.utlis.textdraw.TextDrawable
 
@@ -22,22 +19,20 @@ import com.rh.heji.utlis.textdraw.TextDrawable
 class CategoryTotalAdapter(
     data: MutableList<PieEntry>?
 ) :
-    BaseQuickAdapter<PieEntry, BaseViewHolder>( layoutResId= R.layout.item_category_total, data) {
+    BaseQuickAdapter<PieEntry, BaseViewHolder>(layoutResId = R.layout.item_category_total, data) {
+    val colors = ColorUtils.groupColors()
     private lateinit var itemBinding: ItemCategoryTotalBinding;
-    private val percentFormatter :PercentFormatter=PercentFormatter()
+    private val percentFormatter: PercentFormatter = PercentFormatter()
     override fun convert(holder: BaseViewHolder, item: PieEntry) {
         itemBinding = ItemCategoryTotalBinding.bind(holder.itemView)
-        val drawable = TextDrawable.builder().buildRound(item.label.substring(0, 1), ColorGenerator.MATERIAL.randomColor)
-        itemBinding.imgCategory.setImageDrawable(drawable)
-        itemBinding.tvCategory.text =item.label
-        itemBinding.tvCategoryIncome.text =item.data.toString()
-        itemBinding.tvCategoryPercentage.text ="${percentFormatter.getFormattedValue(item.value)}"
-        val progress = (item.value).toInt()
-        itemBinding.progressBarCategory.progress =progress
-//        val colorGenerator = ClipDrawable(ColorDrawable(ColorGenerator.MATERIAL.randomColor), Gravity.LEFT, ClipDrawable.HORIZONTAL)
-//        itemBinding.progressBarCategory.progressDrawable = colorGenerator //必须先设置到progressbar上再设置level，告诉这个drawable的宽度有多宽，这个level才能生效
-//        drawable.level = progress * 100
-//        itemBinding.progressBarCategory.progressDrawable = colorGenerator
-//        itemBinding.progressBarCategory.progress = progress
+        val textIconColor = colors[holder.layoutPosition]
+        val textIconDrawable = TextDrawable.builder()
+            .buildRound(item.label.substring(0, 1), textIconColor)
+        itemBinding.imgCategory.setImageDrawable(textIconDrawable)
+        itemBinding.tvCategory.text = item.label
+        itemBinding.tvCategoryIncome.text = item.data.toString()
+        itemBinding.tvCategoryPercentage.text = "${percentFormatter.getFormattedValue(item.value)}"
+        val progress = item.value.toInt()
+        itemBinding.progressBarCategory.progress = progress
     }
 }
--- client/app/src/main/java/com/rh/heji/utlis/ColorUtils.kt
+++ client/app/src/main/java/com/rh/heji/utlis/ColorUtils.kt
@@ -1,35 +1,55 @@
-package com.rh.heji.utlis;
+package com.rh.heji.utlis
 
-import android.graphics.Color;
-
-import java.util.Random;
+import android.graphics.Color
+import com.github.mikephil.charting.utils.ColorTemplate
+import java.util.*
 
 /**
  * Date: 2020/9/16
  * Author: 锅得铁
  * #
  */
-public class ColorUtils {
-    public static int randomColor() {
+object ColorUtils {
+    fun randomColor(): Int {
         //随机种子
-        final Random mRandom = new Random(System.currentTimeMillis());
-
-        final int baseRed = Color.red(randomBaseColor());
-        final int baseGreen = Color.green(randomBaseColor());
-        final int baseBlue = Color.blue(randomBaseColor());
-        final int red = (baseRed + mRandom.nextInt(256)) / 2;
-        final int green = (baseGreen + mRandom.nextInt(256)) / 2;
-        final int blue = (baseBlue + mRandom.nextInt(256)) / 2;
-        return Color.rgb(red, green, blue);
+        val mRandom = Random(System.currentTimeMillis())
+        val baseRed = Color.red(randomBaseColor())
+        val baseGreen = Color.green(randomBaseColor())
+        val baseBlue = Color.blue(randomBaseColor())
+        val red = (baseRed + mRandom.nextInt(256)) / 2
+        val green = (baseGreen + mRandom.nextInt(256)) / 2
+        val blue = (baseBlue + mRandom.nextInt(256)) / 2
+        return Color.rgb(red, green, blue)
     }
 
-    public static int randomBaseColor() {
+    fun randomBaseColor(): Int {
         //随机种子
-        final Random mRandom = new Random(System.currentTimeMillis());
-
-        int[] baseColos = new int[]{Color.BLUE, Color.RED, Color.YELLOW, Color.YELLOW, Color.GREEN};
+        val mRandom = Random(System.currentTimeMillis())
+        val baseColos = intArrayOf(
+            Color.BLUE,
+            Color.RED,
+            Color.YELLOW,
+            Color.YELLOW,
+            Color.GREEN
+        )
         // 基础色
-        int baseColor = baseColos[mRandom.nextInt(baseColos.length)];
-        return baseColor;
+        return baseColos[mRandom.nextInt(baseColos.size)]
+    }
+
+    fun groupColors(): ArrayList<Int> {
+        // add a lot of colors
+        val colors = ArrayList<Int>()
+
+        for (c in ColorTemplate.VORDIPLOM_COLORS) colors.add(c)
+
+        for (c in ColorTemplate.JOYFUL_COLORS) colors.add(c)
+
+        for (c in ColorTemplate.COLORFUL_COLORS) colors.add(c)
+
+        for (c in ColorTemplate.LIBERTY_COLORS) colors.add(c)
+
+        for (c in ColorTemplate.PASTEL_COLORS) colors.add(c)
+        colors.add(ColorTemplate.getHoloBlue())
+        return colors
     }
 }

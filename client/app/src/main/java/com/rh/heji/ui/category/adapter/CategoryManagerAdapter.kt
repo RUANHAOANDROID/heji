a3e8bc6adc08ffd906c7ceab51773663f9d269af
ruanhao <52927295@qq.com>
2021-06-15 17:17:37 +0800

    优化代码

--- client/app/src/main/java/com/rh/heji/ui/category/adapter/CategoryManagerAdapter.kt
+++ client/app/src/main/java/com/rh/heji/ui/category/adapter/CategoryManagerAdapter.kt
@@ -1,52 +1,42 @@
-package com.rh.heji.ui.category.adapter;
+package com.rh.heji.ui.category.adapter
 
-import android.text.TextUtils;
-import android.view.View;
-
-import com.chad.library.adapter.base.BaseQuickAdapter;
-import com.chad.library.adapter.base.viewholder.BaseViewHolder;
-import com.rh.heji.R;
-import com.rh.heji.data.AppDatabase;
-import com.rh.heji.data.db.Category;
-import com.rh.heji.data.db.Constant;
-import com.rh.heji.databinding.ItemCategoryManagerBinding;
-import com.rh.heji.utlis.textdraw.ColorGenerator;
-import com.rh.heji.utlis.textdraw.TextDrawable;
-
-import org.jetbrains.annotations.NotNull;
+import com.chad.library.adapter.base.BaseQuickAdapter
+import com.chad.library.adapter.base.viewholder.BaseViewHolder
+import com.rh.heji.R
+import android.text.TextUtils
+import android.view.View
+import com.rh.heji.data.db.Category
+import com.rh.heji.databinding.ItemCategoryManagerBinding
+import com.rh.heji.utlis.textdraw.TextDrawable
 
 /**
  * Date: 2020/9/16
  * Author: 锅得铁
  * #分类适配器
  */
-public class CategoryManagerAdapter extends BaseQuickAdapter<Category, BaseViewHolder> {
-    protected ItemCategoryManagerBinding itemBinding;
-
-    public CategoryManagerAdapter() {
-        super(R.layout.item_category_manager);
-    }
-
-    @Override
-    protected void convert(@NotNull BaseViewHolder holder, Category category) {
-        itemBinding = ItemCategoryManagerBinding.bind(holder.itemView);
-        int bgColor = getContext().getColor(category.selected ? R.color.category_ico_selected : R.color.category_ico);
-        if (TextUtils.isEmpty(category.getCategory())) return;
-        TextDrawable drawable = TextDrawable.builder().buildRound(category.getCategory().substring(0, 1), bgColor);
-        itemBinding.roundImageView.setImageDrawable(drawable);
-        itemBinding.tvName.setText(category.getCategory());
-
-        addChildClickViewIds(itemBinding.btnDelete.getId());
-//        itemBinding.btnDelete.setOnClickListener(v -> {
+open class CategoryManagerAdapter :
+    BaseQuickAdapter<Category, BaseViewHolder>(R.layout.item_category_manager) {
+    @JvmField
+    protected var itemBinding: ItemCategoryManagerBinding? = null
+    override fun convert(holder: BaseViewHolder, category: Category) {
+        itemBinding = ItemCategoryManagerBinding.bind(holder.itemView)
+        val bgColor =
+            context.getColor(if (category.selected) R.color.category_ico_selected else R.color.category_ico)
+        if (TextUtils.isEmpty(category.category)) return
+        val drawable = TextDrawable.builder().buildRound(category.category.substring(0, 1), bgColor)
+        itemBinding!!.roundImageView.setImageDrawable(drawable)
+        itemBinding!!.tvName.text = category.category
+        addChildClickViewIds(itemBinding!!.btnDelete.id)
+        //        itemBinding.btnDelete.setOnClickListener(v -> {
 //            deleteCategory(category);
 //        });
-        if (category.getCategory().equals("其他")){
-            itemBinding.btnEdit.setVisibility(View.INVISIBLE);
-            itemBinding.btnDelete.setVisibility(View.INVISIBLE);
-        }else{
-            itemBinding.btnEdit.setVisibility(View.VISIBLE);
-            itemBinding.btnDelete.setVisibility(View.VISIBLE);
+        //"其他"标签不可删除
+        if (category.category == "其他") {
+            itemBinding!!.btnEdit.visibility = View.INVISIBLE
+            itemBinding!!.btnDelete.visibility = View.INVISIBLE
+        } else {
+            itemBinding!!.btnEdit.visibility = View.VISIBLE
+            itemBinding!!.btnDelete.visibility = View.VISIBLE
         }
     }
-
 }

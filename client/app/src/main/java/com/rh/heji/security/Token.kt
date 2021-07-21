020e82642c63c26d83c5f71f72d4e3b71fbb5462
ruanhao <52927295@qq.com>
2021-07-21 17:46:55 +0800

    token 测试时给默认TOKEN

--- client/app/src/main/java/com/rh/heji/security/Token.kt
+++ client/app/src/main/java/com/rh/heji/security/Token.kt
@@ -4,40 +4,41 @@ import android.content.Context
 import android.text.TextUtils
 import com.blankj.utilcode.util.EncodeUtils
 import com.blankj.utilcode.util.FileUtils
+import com.rh.heji.TOKEN
 import java.io.*
 import java.nio.charset.StandardCharsets
 
 class Token(val context: Context) {
     // Error occurred when opening raw file for reading.
-    val tokenString: String
-        get() {
-            val tokenFile = File(context.filesDir, "TokenFile")
-            var token = ""
-            if (tokenFile.exists()) {
-                try {
-                    val inputStream = FileInputStream(tokenFile)
-                    val inputStreamReader = InputStreamReader(inputStream, StandardCharsets.UTF_8)
-                    val stringBuilder = StringBuilder()
-                    try {
-                        BufferedReader(inputStreamReader).use { reader ->
-                            var line = reader.readLine()
-                            while (line != null) {
-                                stringBuilder.append(line)
-                                line = reader.readLine()
-                            }
-                        }
-                    } catch (e: IOException) {
-                        // Error occurred when opening raw file for reading.
-                        e.printStackTrace()
-                    } finally {
-                        token = stringBuilder.toString()
-                    }
-                } catch (e: FileNotFoundException) {
-                    e.printStackTrace()
-                }
-            }
-            return String(EncodeUtils.base64Decode(token))
-        }
+    val tokenString: String= TOKEN
+//        get() {
+//            val tokenFile = File(context.filesDir, "TokenFile")
+//            var token = ""
+//            if (tokenFile.exists()) {
+//                try {
+//                    val inputStream = FileInputStream(tokenFile)
+//                    val inputStreamReader = InputStreamReader(inputStream, StandardCharsets.UTF_8)
+//                    val stringBuilder = StringBuilder()
+//                    try {
+//                        BufferedReader(inputStreamReader).use { reader ->
+//                            var line = reader.readLine()
+//                            while (line != null) {
+//                                stringBuilder.append(line)
+//                                line = reader.readLine()
+//                            }
+//                        }
+//                    } catch (e: IOException) {
+//                        // Error occurred when opening raw file for reading.
+//                        e.printStackTrace()
+//                    } finally {
+//                        token = stringBuilder.toString()
+//                    }
+//                } catch (e: FileNotFoundException) {
+//                    e.printStackTrace()
+//                }
+//            }
+//            return String(EncodeUtils.base64Decode(token))
+//        }
 
     fun save(token: String?) {
         val fileName = "TokenFile"

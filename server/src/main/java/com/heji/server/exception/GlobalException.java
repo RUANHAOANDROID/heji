52f82abc1b552c179879b19c75245fb2e218c7f2
ruanhao <52927295@qq.com>
2021-09-01 20:16:44 +0800

    优化异常处理

--- server/src/main/java/com/heji/server/exception/GlobalException.java
+++ server/src/main/java/com/heji/server/exception/GlobalException.java
@@ -2,7 +2,7 @@ package com.heji.server.exception;
 
 public class GlobalException extends RuntimeException {
 
-    private Integer code = 500; //因为我需要将异常信息也返回给接口中，所以添加code区分
+    private Integer code = -1; //因为我需要将异常信息也返回给接口中，所以添加code区分
 
     public GlobalException(Integer code, String message) {
         super(message);  //把自定义的message传递个异常父类

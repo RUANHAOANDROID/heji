0ccacd8c28891f638774fbfabbb370f48f034f34
阮皓 <52927295@qq.com>
2021-11-18 19:57:23 +0800

    精简同步日志接口，按账本同步

--- server/src/main/java/com/heji/server/controller/OperateLogController.java
+++ server/src/main/java/com/heji/server/controller/OperateLogController.java
@@ -24,19 +24,9 @@ public class OperateLogController {
      * @return 操作日志
      */
     @ResponseBody
-    @GetMapping(value = {"/delete"}, produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
-    public String getDeleteLog(@RequestParam String bookId) {
+    @GetMapping(value = {"/bookLogs"}, produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
+    public String getOperateLogs(@RequestParam String bookId) {
         List<MOperateLog> deleteLog = operateLogService.getOperateLogs(bookId);
-        return Result.success(deleteLog);
-    }
-    /**
-     * @param bookId 账本ID
-     * @return 操作日志
-     */
-    @ResponseBody
-    @GetMapping(value = {"/update"}, produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
-    public String getUpdateLog(@RequestParam String bookId) {
-        List<MOperateLog> deleteLog = operateLogService.getUpdateLog(bookId,"2021-01-01","2021-09-07");
         return Result.success(deleteLog);
     }
 }
--- server/src/main/java/com/heji/server/service/OperateLogService.java
+++ server/src/main/java/com/heji/server/service/OperateLogService.java
@@ -5,13 +5,6 @@ import com.heji.server.data.mongo.MOperateLog;
 import java.util.List;
 
 public interface OperateLogService {
-
     void addOperateLog(MOperateLog object);
-
-    List<MOperateLog> getDeleteLog(String book_id, String startDate, String endDate);
-
     List<MOperateLog> getOperateLogs(String book_id);
-
-    List<MOperateLog> getUpdateLog(String book_id, String startDate, String endDate);
-
 }

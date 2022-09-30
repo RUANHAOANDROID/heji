97db25c11ebc8123cbe69e9d953a5d9608f28f6e
hao <52927295@qq.com>
2022-10-01 00:46:58 +0800

    重构导出模块为mvi

--- client/app/src/main/java/com/rh/heji/ui/user/info/UserInfoFragment.kt
+++ client/app/src/main/java/com/rh/heji/ui/user/info/UserInfoFragment.kt
@@ -1,15 +1,11 @@
 package com.rh.heji.ui.user.info
 
-import android.text.Spannable
-import android.text.SpannableString
-import android.text.style.ForegroundColorSpan
 import android.view.View
 import androidx.lifecycle.ViewModelProvider
 import com.rh.heji.App
 import com.rh.heji.R
 import com.rh.heji.databinding.FragmentUserInfoBinding
 import com.rh.heji.ui.base.BaseFragment
-import com.rh.heji.ui.user.JWTParse
 
 class UserInfoFragment : BaseFragment() {
     private val viewModel by lazy { ViewModelProvider(this)[UserInfoViewModel::class.java] }

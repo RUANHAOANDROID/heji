cb96fd8345eac3dea60699f1a600a9211c3ea394
hao <52927295@qq.com>
2022-10-01 22:27:27 +0800

    优化账单列表防抖处理

---
+++ client/app/src/main/java/com/rh/heji/utlis/ClickUtils.kt
@@ -0,0 +1,20 @@
+package com.rh.heji.utlis
+
+/**
+ *
+ * @date 2022/10/1
+ * @author 锅得铁
+ * @since v1.0
+ */
+object ClickUtils {
+    private const val FAST_CLICK_DELAY_TIME = 1000
+    private var lastClickTime = 0L
+
+    fun debouncing(doAction: () -> Unit) {
+        if (System.currentTimeMillis() - lastClickTime >= FAST_CLICK_DELAY_TIME) {
+            lastClickTime = System.currentTimeMillis()
+            doAction()
+        }
+    }
+
+}

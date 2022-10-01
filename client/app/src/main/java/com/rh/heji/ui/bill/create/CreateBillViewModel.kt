690c2ca4084e6248832507b93874cff17dcf76f3
hao <52927295@qq.com>
2022-10-01 21:56:38 +0800

    优化记账代码结构

--- client/app/src/main/java/com/rh/heji/ui/bill/create/CreateBillViewModel.kt
+++ client/app/src/main/java/com/rh/heji/ui/bill/create/CreateBillViewModel.kt
@@ -6,61 +6,59 @@ import com.rh.heji.App
 import com.rh.heji.data.db.*
 import com.rh.heji.data.db.mongo.ObjectId
 import com.rh.heji.service.sync.IBillSync
-import com.rh.heji.ui.base.BaseViewModel
+import com.rh.heji.ui.base.BaseViewModelMVI
 import com.rh.heji.utlis.launchIO
 import java.util.*
 
 /**
  * 账单添加页ViewModel 不要在其他页面应用该ViewModel
  */
-class CreateBillViewModel(private val mBillSync: IBillSync) : BaseViewModel() {
-
-    private val uiStateLiveData = MutableLiveData<CreateBillUIState>()
-
-    fun subUIState(): LiveData<CreateBillUIState> {
-        return uiStateLiveData
-    }
+class CreateBillViewModel(private val mBillSync: IBillSync) :
+    BaseViewModelMVI<CreateBillAction, CreateBillUIState>() {
 
     var keyBoardStack: Stack<String>? = null//用于保存栈
 
-    fun eventState(event: CreateBillEvent) = launchIO({
-        when (event) {
-            is CreateBillEvent.Save -> {
-                save(event.bill)
-                uiStateLiveData.postValue(CreateBillUIState.Close)
-            }
-            is CreateBillEvent.SaveAgain -> {
-                save(event.bill)
-                uiStateLiveData.postValue(CreateBillUIState.Reset)
-            }
-
-            is CreateBillEvent.GetBill -> {
-                event.bill_id?.let {
-                    val bill = App.dataBase.billImageDao().findBillAndImage(it)
-                    uiStateLiveData.postValue(CreateBillUIState.BillChange(bill = bill))
+    override fun doAction(action: CreateBillAction) {
+        super.doAction(action)
+        launchIO({
+            when (action) {
+                is CreateBillAction.Save -> {
+                    save(action.bill)
+                    uiState.postValue(CreateBillUIState.Close)
+                }
+                is CreateBillAction.SaveAgain -> {
+                    save(action.bill)
+                    uiState.postValue(CreateBillUIState.Reset)
                 }
 
+                is CreateBillAction.GetBill -> {
+                    action.bill_id?.let {
+                        val bill = App.dataBase.billImageDao().findBillAndImage(it)
+                        uiState.postValue(CreateBillUIState.BillChange(bill = bill))
+                    }
+
+                }
+                is CreateBillAction.GetDealers -> {
+                    val users = App.dataBase.dealerDao().findAll().map {
+                        it.userName
+                    }.toMutableList()
+                    uiState.postValue(CreateBillUIState.Dealers(users))
+                }
+                is CreateBillAction.GetImages -> {
+                    val images = App.dataBase.imageDao().findImage(action.img_ids)
+                    uiState.postValue(CreateBillUIState.Images(images))
+                }
+                is CreateBillAction.DeleteImage -> {
+                    val image = action.image
+                    App.dataBase.imageDao().preDelete(image.id)
+                    mBillSync.deleteImage(image)
+                }
             }
-            is CreateBillEvent.GetDealers -> {
-                val users = App.dataBase.dealerDao().findAll().map {
-                    it.userName
-                }.toMutableList()
-                uiStateLiveData.postValue(CreateBillUIState.Dealers(users))
-            }
-            is CreateBillEvent.GetImages -> {
-                val images = App.dataBase.imageDao().findImage(event.img_ids)
-                uiStateLiveData.postValue(CreateBillUIState.Images(images))
-            }
-            is CreateBillEvent.DeleteImage->{
-                val image =event.image
-                App.dataBase.imageDao().preDelete(image.id)
-                mBillSync.deleteImage(image )
-            }
-        }
-    }, {
-        uiStateLiveData.postValue(CreateBillUIState.Error(it))
-        ToastUtils.showLong(it.message)
-    })
+        }, {
+            uiState.postValue(CreateBillUIState.Error(it))
+            ToastUtils.showLong(it.message)
+        })
+    }
 
     /**
      * 保存账单到本地

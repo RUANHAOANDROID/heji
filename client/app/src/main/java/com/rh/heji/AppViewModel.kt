93243b34db809a240b97fc81ee174d80039916f6
阮皓 <52927295@qq.com>
2022-11-13 00:40:49 +0800

    删除冗余代码

--- client/app/src/main/java/com/rh/heji/AppViewModel.kt
+++ client/app/src/main/java/com/rh/heji/AppViewModel.kt
@@ -4,22 +4,15 @@ import android.app.Application
 import android.content.Context
 import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.MediatorLiveData
-import androidx.room.Entity
 import com.blankj.utilcode.util.LogUtils
-import com.rh.heji.data.*
 import com.rh.heji.data.db.Bill
 import com.rh.heji.data.db.Book
 import com.rh.heji.data.db.Category
-import com.rh.heji.data.db.STATUS
-import com.rh.heji.data.repository.BillRepository
-import com.rh.heji.data.repository.BookRepository
 import com.rh.heji.service.work.DataSyncWork
 import com.rh.heji.utlis.launchIO
-import com.rh.heji.utlis.launchNewThread
 
 class AppViewModel(application: Application) : AndroidViewModel(application) {
-    private val billRepository = BillRepository()
-    val localDataEvent = MediatorLiveData<EventMessage>()
+
     val loginEvent = MediatorLiveData<Event<Any>>()
 
     init {
@@ -28,24 +21,6 @@ class AppViewModel(application: Application) : AndroidViewModel(application) {
         }, {
             it.printStackTrace()
         })
-        localDataEvent.observeForever {
-            val data = it.entity
-            val optType = it.crud
-
-            launchIO({
-                if (data is Book) {
-                    BookTask(optType, data).sync()
-                }
-                if (data is Bill) {
-                    BillTask(optType, data).sync()
-                }
-                if (data is Category) {
-                    CategoryTask(optType, data).sync()
-                }
-            }, { error ->
-                error.printStackTrace()
-            })
-        }
     }
 
     fun asyncData() {

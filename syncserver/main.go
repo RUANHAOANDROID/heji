dd0542d8d09f7bc0e86191124411be41a67da732
hao88 <52927295@qq.com>
2023-08-30 22:00:19 +0800

    完善服务器启动

--- syncserver/main.go
+++ syncserver/main.go
@@ -1,4 +1,16 @@
 package main
 
+import (
+	"syncserver/api"
+	"syncserver/config"
+	"syncserver/pkg"
+)
+
 func main() {
+	conf, err := config.Load("config.yml")
+	if err != nil {
+		pkg.Log.Error(err.Error())
+		panic(err)
+	}
+	api.Start(conf)
 }

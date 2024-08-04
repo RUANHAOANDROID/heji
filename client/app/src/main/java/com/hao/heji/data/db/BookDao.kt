5ede1ddea4ee662c5361e3bef7aa65e551113624
hao88 <52927295@qq.com>
2024-08-04 16:45:48 +0800

    语句优化

--- client/app/src/main/java/com/hao/heji/data/db/BookDao.kt
+++ client/app/src/main/java/com/hao/heji/data/db/BookDao.kt
@@ -12,34 +12,34 @@ interface BookDao {
     @Update(entity = Book::class, onConflict = OnConflictStrategy.REPLACE)
     fun update(book: Book): Int
 
-    @Query("delete from book  where book_id=:bookId")
+    @Query("DELETE FROM book  WHERE book_id=:bookId")
     fun deleteById(bookId: String)
 
-    @Query("update book set sync_status = ${STATUS.DELETED} where book_id=:bookId")
-    fun preDelete(bookId: String): Int
+    @Query("UPDATE book SET sync_status =:status WHERE book_id=:bookId")
+    fun preDelete(bookId: String,status:Int=STATUS.DELETED): Int
 
     @Query("select count() from book  where name=:name")
     fun countByName(name: String): Int
 
-    @Query("SELECT * FROM book WHERE sync_status!=${STATUS.DELETED} ORDER BY book_id")
-    fun allBooks(): Flow<MutableList<Book>>
+    @Query("SELECT * FROM book WHERE sync_status!=:status ORDER BY book_id")
+    fun allBooks(status: Int=STATUS.DELETED): Flow<MutableList<Book>>
 
-    @Query("select * from book where crt_user_id=:userID and is_initial == 1")
+    @Query("SELECT * FROM book WHERE crt_user_id=:userID AND is_initial == 1")
     fun findInitBook(userID: String): MutableList<Book>
 
-    @Query("select is_initial from book where book_id=:bookId")
+    @Query("SELECT is_initial FROM book WHERE book_id=:bookId")
     fun isInitialBook(bookId: String): Int
 
-    @Query("select * from book where sync_status!=:status")
+    @Query("SELECT * FROM book WHERE sync_status!=:status")
     fun flowNotSynced(status: Int=STATUS.SYNCED):Flow<MutableList<Book>>
 
-    @Query("select count(0) from book")
+    @Query("SELECT count(0) FROM book")
     fun count(): Int
 
-    @Query("select count(0) from book WHERE book_id=:bookId")
+    @Query("SELECT count(0) FROM book WHERE book_id=:bookId")
     fun exist(bookId: String): Int
 
-    @Query("select * from book where book_id =:id")
+    @Query("SELECT * FROM book WHERE book_id =:id")
     fun findBook(id: String): MutableList<Book>
 
     @Query("SELECT book_id FROM book WHERE crt_user_id=:crtUserId")
